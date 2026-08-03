3f4f2905909f7d18f01be1b32f2a5eb57b3f309b
nslmanu <nslmanu@me.com>
2026-08-03 10:37:43 +0200

    first commit

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 =  # your code here
+arr1 =  arr[:2]
 arr2 =  # your code here
 arr3 =  # your code here
 print("The first two numbers of the array are: {}".format(arr1))
