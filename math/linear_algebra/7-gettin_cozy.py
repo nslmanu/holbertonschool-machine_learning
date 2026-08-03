614b58fdb03de5bdb52967456220469e9ed648be
nslmanu <nslmanu@me.com>
2026-08-03 16:15:41 +0200

    first commit

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""def cat_matrices2D(mat1, mat2, axis=0): that concatenates two matrices along a specific axis:"""
+
+
+def cat_matrices2D(arr1, arr2):
+    """Add arr1 and arr2 to new element-wise.
+
+    Return a new list.
+    """
+
+    result = []
+    result = (arr1 + arr2)
+    return result
