e3347a2d5ec8a26d1634c9a7846b95e3a3019db8
nslmanu <nslmanu@me.com>
2026-08-03 12:23:22 +0200

    first commit

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-"""Module qui calcule la shape d'une matrice."""
+"""Write a function def matrix_transpose(matrix):
+ that returns each X of a 2D matrix, mat:"""
 
 
-def matrix_shape(datamat):
-    """Retourne la shape d'une matrice sous forme de liste d'entiers."""
-    shape = []
-    while isinstance(datamat, list):
-        shape.append(len(datamat))
-        datamat = datamat[0]
-    return shape
+def matrix_transpose(datamat):
+    """take 1st of each and insert and new, etc etc"""
+    result = []
+    for j in range(len(datamat[0])):
+        new_row = []
+        for i in range(len(datamat)):
+            new_row.append(datamat[i][j])
+        result.append(new_row)
+    return result
