c497072ff46863e28c2148d38369c0ccfc7fb5f0
nslmanu <nslmanu@me.com>
2026-08-04 11:03:37 +0200

    first commit

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -2,35 +2,22 @@
 """mat_mul(mat1, mat2): that performs matrix multiplication:"""
 
 
-def mat_mul(mat1, mat2, axis=0):
+def mat_mul(mat1, mat2):
     """mult arr1 and arr2 to new element-wise.
 
     Return a new list. hori and vert
     """
+    if len(mat1[0]) != len(mat2):
+        return None
 
-    if axis == 0:
-        if len(mat1[0]) != len(mat2[0]):
-            return None
+    result = []
 
-        result = []
-        for row in mat1:
-            result.append(row[:])
-        for row in mat2:
-            result.append(row[:])
-        return result
-
-    if axis == 1:
-        if len(mat1) != len(mat2):
-            return None
-
-        result = []
-        for i in range(len(mat1)):
-            new_row = []
-            for j in range(len(mat1[i])):
-                new_row.append(mat1[i][j])
-            for j in range(len(mat2[i])):
-                new_row.append(mat2[i][j])
-            result.append(new_row)
-        return result
-
-    return None
+    for each_lines in range(len(mat1)):
+        new_line = []
+        for each_col in range(len(mat2[0])):
+            total = 0
+            for each_linesBIS in range(len(mat2)):
+                total += mat1[each_lines][each_linesBIS] * mat2[each_linesBIS][each_col]
+            new_line.append(total)
+        result.append(new_line)
+    return result
