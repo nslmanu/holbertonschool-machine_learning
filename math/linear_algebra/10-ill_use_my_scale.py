d45712c7b4b8ad7c072e22b361bbcc5afe421a2f
nslmanu <nslmanu@me.com>
2026-08-04 13:35:44 +0200

    first commit

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,24 +1,7 @@
 #!/usr/bin/env python3
-"""mat_mul(mat1, mat2): that performs matrix multiplication:"""
+"""calculate size shape of a numpy ndarray."""
 
 
-def mat_mul(mat1, mat2):
-    """mult arr1 and arr2 to new element-wise.
-
-    Return a new list. hori and vert
-    """
-    if len(mat1[0]) != len(mat2):
-        return None
-
-    result = []
-
-    for each_lines in range(len(mat1)):
-        new_line = []
-        for each_col in range(len(mat2[0])):
-            total = 0
-            for each_linesBIS in range(len(mat2)):
-                total += mat1[each_lines][each_linesBIS] \
-                    * mat2[each_linesBIS][each_col]
-            new_line.append(total)
-        result.append(new_line)
-    return result
+def np_shape(matrix):
+    """size shape of a numpy ndarray for a tuple of int"""
+    return matrix.shape
