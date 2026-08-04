b88a0f59e2a69630f5a74d55943850070fcefaae
nslmanu <nslmanu@me.com>
2026-08-04 09:45:10 +0200

    first commit

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
-"""def cat_matrices2D(mat1, mat2, axis=0): that concatenates two matrices along a specific axis:"""
+"""def cat_matrices2D(mat1, mat2, axis=0): that concatenates
+two matrices along a specific axis:"""
 
 
-def cat_matrices2D(arr1, arr2):
+def cat_matrices2D(mat1, mat2, direction=0):
     """Add arr1 and arr2 to new element-wise.
 
-    Return a new list.
+    Return a new list. hori and vert
     """
 
-    result = []
-    result = (arr1 + arr2)
-    return result
+    if direction == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+
+        result = []
+        for row in mat1:
+            result.append(row[:])
+        for row in mat2:
+            result.append(row[:])
+        return result
+
+    if direction == 1:
+        if len(mat1) != len(mat2):
+            return None
+
+        result = []
+        for i in range(len(mat1)):
+            new_row = []
+            for j in range(len(mat1[i])):
+                new_row.append(mat1[i][j])
+            for j in range(len(mat2[i])):
+                new_row.append(mat2[i][j])
+            result.append(new_row)
+        return result
+
+    return None
