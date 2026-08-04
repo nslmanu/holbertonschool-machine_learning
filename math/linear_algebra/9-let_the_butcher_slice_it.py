332b94ab615b80e4d7633ea52e75ce9b1a81ba73
nslmanu <nslmanu@me.com>
2026-08-04 11:16:33 +0200

    first commit

---
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""mat_mul(mat1, mat2): that performs matrix multiplication:"""
+
+
+def mat_mul(mat1, mat2):
+    """mult arr1 and arr2 to new element-wise.
+
+    Return a new list. hori and vert
+    """
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    result = []
+
+    for each_lines in range(len(mat1)):
+        new_line = []
+        for each_col in range(len(mat2[0])):
+            total = 0
+            for each_linesBIS in range(len(mat2)):
+                total += mat1[each_lines][each_linesBIS] \
+                    * mat2[each_linesBIS][each_col]
+            new_line.append(total)
+        result.append(new_line)
+    return result
