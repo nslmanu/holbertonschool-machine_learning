5f0f35ff77a59b105e17d117e16e48beba63cfb3
nslmanu <nslmanu@me.com>
2026-08-04 11:08:22 +0200

    first commit

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -17,7 +17,8 @@ def mat_mul(mat1, mat2):
         for each_col in range(len(mat2[0])):
             total = 0
             for each_linesBIS in range(len(mat2)):
-                total += mat1[each_lines][each_linesBIS] * mat2[each_linesBIS][each_col]
+                total += mat1[each_lines][each_linesBIS] \
+                    * mat2[each_linesBIS][each_col]
             new_line.append(total)
         result.append(new_line)
     return result
