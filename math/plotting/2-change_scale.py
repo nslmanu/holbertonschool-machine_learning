93a8f2bed465aec0d57b192f531604be65c4fbca
nslmanu <nslmanu@me.com>
2026-08-07 15:22:54 +0200

    first commit

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
+"""Module that plots exponential decay of C-14."""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def change_scale():
+    """Plot exponential decay of C-14 with logarithmic y-axis."""
     x = np.arange(0, 28651, 5730)
     r = np.log(0.5)
     t = 5730
     y = np.exp((r / t) * x)
     plt.figure(figsize=(6.4, 4.8))
-
-
     plt.plot(x, y)
     plt.yscale('log')
     plt.xlabel('Time (years)')
