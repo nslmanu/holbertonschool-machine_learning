fb2079afe0b9481eca3873be47de69b21fedb297
nslmanu <nslmanu@me.com>
2026-08-07 14:15:45 +0200

    first commit

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -6,5 +6,10 @@ def line():
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
-    # your code here
+    plt.plot(np.arange(0, 11), y, 'r-')   # 'r-' = rouge, ligne continue
+    plt.xlim(0, 10)                         # axe x : 0 à 10
+    plt.ylim(0, y[-1])                      # axe y : 0 au dernier point (1000)
+    plt.gca().set_facecolor('white')        # fond du graphique blanc
+    plt.gcf().set_facecolor('white')        # fond de la figure blanc
+    plt.show()
+    
