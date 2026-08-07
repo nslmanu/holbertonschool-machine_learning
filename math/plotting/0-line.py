bcd523390cd4bbd1852238a331437a38d15c1d37
nslmanu <nslmanu@me.com>
2026-08-07 14:22:39 +0200

    first commit

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3
+"""Module that plots y as a line graph."""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """Plot y = x^3 as a solid red line."""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
