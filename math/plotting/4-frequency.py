df475400a19bad71a182a27c99888be8095c3f9a
nslmanu <nslmanu@me.com>
2026-08-17 09:16:37 +0200

    first commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 
 
 def frequency():
-    """Plot a histogram of student grades."""
+    """Plot a histogram of student grades for Project A."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, bins=10, edgecolor='black')
+    plt.hist(student_grades,
+             bins=range(0, 101, 10),
+             edgecolor='black')
+    plt.xlim(0, 100)
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.xlim(0, 100)
     plt.show()
