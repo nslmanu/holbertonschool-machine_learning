ad05e85aff6272be217b4ba405f140f8fde084bd
nslmanu <nslmanu@me.com>
2026-08-17 09:28:06 +0200

    first commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -10,8 +10,8 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
     plt.hist(student_grades,
-             bins=range(0, 101, 10),
-             edgecolor='black')
+         bins=range(0, 101, 10),
+         edgecolor='k')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
