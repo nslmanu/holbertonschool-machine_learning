0ed1c0c9f7e9e4fba57cbe2e4ac9c4535dc9b931
nslmanu <nslmanu@me.com>
2026-08-19 11:39:33 +0200

    first commit

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -278,10 +278,10 @@ class Decision_Tree():
 
         if verbose == 1:
             print(f"""    Training finished.
-    - Depth : {self.depth()}
-    - Number of nodes : {self.count_nodes()}
-    - Number of leaves : {self.count_nodes(only_leaves=True)}
-    - Accuracy on training data : {self.accuracy
+        - Depth : {self.depth()}
+        - Number of nodes : {self.count_nodes()}
+        - Number of leaves : {self.count_nodes(only_leaves=True)}
+        - Accuracy on training data : {self.accuracy
                                    (self.explanatory, self.target)}""")
 
     def accuracy(self, test_explanatory, test_target):
