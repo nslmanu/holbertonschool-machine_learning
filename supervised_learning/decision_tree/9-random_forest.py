bc57f0b0e0c76191f805d285353ed035fb6877eb
nslmanu <nslmanu@me.com>
2026-08-20 09:39:04 +0200

    first commit

---
+++ supervised_learning/decision_tree/9-random_forest.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Module that implements a Random Forest classifier."""
+import numpy as np
+Decision_Tree = __import__('8-build_decision_tree').Decision_Tree
+
+
+class Random_Forest():
+    """Represents a Random Forest classifier."""
+
+    def __init__(self, n_trees=100, max_depth=10, min_pop=1, seed=0):
+        """Initialize a Random_Forest."""
+        self.numpy_predicts = []
+        self.target = None
+        self.numpy_preds = None
+        self.n_trees = n_trees
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.seed = seed
+
+    def predict(self, explanatory):
+        """Predict class for each individual using majority vote."""
+        predictions = np.array([
+            pred(explanatory) for pred in self.numpy_predicts
+        ])
+        return np.array([
+            np.bincount(predictions[:, i].astype(int)).argmax()
+            for i in range(explanatory.shape[0])
+        ])
+
+    def fit(self, explanatory, target, n_trees=100, verbose=0):
+        """Fit the random forest."""
+        self.target = target
+        self.explanatory = explanatory
+        self.numpy_preds = []
+        depths = []
+        nodes = []
+        leaves = []
+        accuracies = []
+
+        for i in range(n_trees):
+            T = Decision_Tree(max_depth=self.max_depth,
+                              min_pop=self.min_pop,
+                              seed=self.seed + i)
+            T.fit(explanatory, target)
+            self.numpy_predicts.append(T.predict)
+            depths.append(T.depth())
+            nodes.append(T.count_nodes())
+            leaves.append(T.count_nodes(only_leaves=True))
+            accuracies.append(T.accuracy(T.explanatory, T.target))
+
+        if verbose == 1:
+            print(f"""  Training finished.
+    - Mean depth                      : {np.array(depths).mean()     }
+    - Mean number of nodes            : {np.array(nodes).mean()      }
+    - Mean number of leaves           : {np.array(leaves).mean()     }
+    - Mean accuracy on training data  : {np.array(accuracies).mean() }
+    - Accuracy of the forest on td    : {self.accuracy(self.explanatory, self.target)}""")
+
+    def accuracy(self, test_explanatory, test_target):
+        """Return accuracy of the forest."""
+        return np.sum(
+            np.equal(self.predict(test_explanatory),
+                     test_target)) / test_target.size
