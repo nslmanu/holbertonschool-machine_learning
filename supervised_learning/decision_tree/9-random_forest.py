5c08e5f76f81a1caafda856b72028fef3b01936e
nslmanu <nslmanu@me.com>
2026-08-20 09:44:04 +0200

    first commit

--- supervised_learning/decision_tree/9-random_forest.py
+++ supervised_learning/decision_tree/9-random_forest.py
@@ -50,12 +50,12 @@ class Random_Forest():
 
         if verbose == 1:                              # ← 8 espaces
             print(f"""  Training finished.
-    - Mean depth                     : {np.array(depths).mean()     }
-    - Mean number of nodes           : {np.array(nodes).mean()      }
-    - Mean number of leaves          : {np.array(leaves).mean()     }
-    - Mean accuracy on training data : {np.array(accuracies).mean() }
-    - Accuracy of the forest on td   : {self.accuracy(self.explanatory, 
-                                                      self.target)}""")
+    - Mean depth                     : {np.array(depths).mean()}
+    - Mean number of nodes           : {np.array(nodes).mean()}
+    - Mean number of leaves          : {np.array(leaves).mean()}
+    - Mean accuracy on training data : {np.array(accuracies).mean()}
+    - Accuracy of the forest on td   : 
+    {self.accuracy(self.explanatory, self.target)}""")
 
     def accuracy(self, test_explanatory, test_target):
         """Return accuracy of the forest."""
