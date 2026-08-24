c8a544ddaa5deabd4e4a9019edc8680f34c8817c
nslmanu <nslmanu@me.com>
2026-08-24 16:07:35 +0200

    first commit

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""Module that defines a single neuron performing binary classification."""
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification."""
+
+    def __init__(self, nx):
+        """Initialize a Neuron.
+
+        Args:
+            nx (int): the number of input features to the neuron.
+
+        Raises:
+            TypeError: if nx is not an integer.
+            ValueError: if nx is less than 1.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculate the forward propagation of the neuron.
+
+        Args:
+            X (numpy.ndarray): input data of shape (nx, m).
+
+        Returns:
+            numpy.ndarray: the activated output __A (sigmoid).
+        """
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculate the cost of the model using logistic regression.
+
+        Args:
+            Y (numpy.ndarray): correct labels of shape (1, m).
+            A (numpy.ndarray): activated output of shape (1, m).
+
+        Returns:
+            float: the cross-entropy cost.
+        """
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
